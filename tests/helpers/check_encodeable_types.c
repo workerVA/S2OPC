3d410a6b97e2691e84223cc2f30f27783b72b8fc
Matthieu Archez <matthieu.archez@systerel.fr>
2019-02-12 17:45:46 +0100

    Ticket #565: correcting with LVO remarks

--- tests/helpers/check_encodeable_types.c
+++ tests/helpers/check_encodeable_types.c
@@ -19,7 +19,7 @@
 
 /** \file
  *
- * \brief Entry point for threads tests. Tests use libcheck.
+ * \brief Entry point for encodeable types tests. Tests use libcheck.
  *
  * If you want to debug the exe, you should define env var CK_FORK=no
  * http://check.sourceforge.net/doc/check_html/check_4.html#No-Fork-Mode
@@ -28,11 +28,18 @@
 #include <check.h>
 #include <stdlib.h>
 
+#include "check_encodeable_types.h"
 #include "check_helpers.h"
 
 #include "sopc_encodeable.h"
 #include "sopc_helper_endianness_cfg.h"
 #include "sopc_types.h"
+
+void setup(void)
+{
+    // Endianness init
+    SOPC_Helper_EndiannessCfg_Initialize();
+}
 
 /**
  * TimeZoneDataType unitary test
@@ -40,45 +47,42 @@
  */
 START_TEST(test_time_zone_data_type)
 {
-    // Endianness init
-    SOPC_Helper_EndiannessCfg_Initialize();
-
-    OpcUa_TimeZoneDataType* t_time_zone_date_type = (OpcUa_TimeZoneDataType*) malloc(sizeof(OpcUa_TimeZoneDataType));
-    SOPC_ReturnStatus t_status = SOPC_STATUS_INVALID_PARAMETERS;
-
-    // Buffer initialization
-    SOPC_Buffer* t_buffer_input = SOPC_Buffer_Create(3);
-    SOPC_Buffer* t_buffer_output = SOPC_Buffer_Create(3);
+    OpcUa_TimeZoneDataType* time_zone_data_type = malloc(sizeof *time_zone_data_type);
+    SOPC_ReturnStatus return_status = SOPC_STATUS_OK;
 
     // Test trame creation (with cursor position reset)
-    uint8_t t_trame[] = {0xFF, 0xFF, 0x1};
-    size_t trame_size = (int) sizeof(t_trame);
+    uint8_t trame[] = {0xFF, 0xFF, 0x1};
+    size_t trame_size = sizeof trame;
 
-    t_status = SOPC_Buffer_Write(t_buffer_input, t_trame, (uint32_t) trame_size);
-    ck_assert(t_status == SOPC_STATUS_OK);
-    t_status = SOPC_Buffer_SetPosition(t_buffer_input, 0);
-    ck_assert(t_status == SOPC_STATUS_OK);
+    // Buffer initialization
+    SOPC_Buffer* buffer_input = SOPC_Buffer_Create((uint32_t) trame_size);
+    SOPC_Buffer* buffer_output = SOPC_Buffer_Create((uint32_t) trame_size);
+
+    return_status = SOPC_Buffer_Write(buffer_input, trame, (uint32_t) trame_size);
+    ck_assert(return_status == SOPC_STATUS_OK);
+    return_status = SOPC_Buffer_SetPosition(buffer_input, 0);
+    ck_assert(return_status == SOPC_STATUS_OK);
 
     // Initialization
-    OpcUa_TimeZoneDataType_EncodeableType.Initialize(t_time_zone_date_type);
+    OpcUa_TimeZoneDataType_EncodeableType.Initialize(time_zone_data_type);
 
     // Decode
-    t_status = OpcUa_TimeZoneDataType_EncodeableType.Decode(t_time_zone_date_type, t_buffer_input);
-    ck_assert(t_status == SOPC_STATUS_OK);
+    return_status = OpcUa_TimeZoneDataType_EncodeableType.Decode(time_zone_data_type, buffer_input);
+    ck_assert(return_status == SOPC_STATUS_OK);
 
     // Check content of encodeable type
-    ck_assert(t_time_zone_date_type->Offset == -1);                   // 0xFF FF -> -1
-    ck_assert(t_time_zone_date_type->DaylightSavingInOffset == true); // 0x1 -> true
+    ck_assert(time_zone_data_type->Offset == -1);                   // 0xFF FF -> -1
+    ck_assert(time_zone_data_type->DaylightSavingInOffset == true); // 0x1 -> true
 
     // Encode
-    t_status = OpcUa_TimeZoneDataType_EncodeableType.Encode(t_time_zone_date_type, t_buffer_output);
-    ck_assert(t_status == SOPC_STATUS_OK);
+    return_status = OpcUa_TimeZoneDataType_EncodeableType.Encode(time_zone_data_type, buffer_output);
+    ck_assert(return_status == SOPC_STATUS_OK);
 
     // Check buffers
-    ck_assert(memcmp(t_buffer_input->data, t_buffer_output->data, trame_size) == 0);
+    ck_assert(memcmp(buffer_input->data, buffer_output->data, trame_size) == 0);
 
     // Clear
-    OpcUa_TimeZoneDataType_EncodeableType.Clear(t_time_zone_date_type);
+    OpcUa_TimeZoneDataType_EncodeableType.Clear(time_zone_data_type);
 }
 END_TEST
 
@@ -90,6 +94,9 @@ Suite* tests_make_suite_encodeable_types(void)
     s = suite_create("Tests for encodeable types");
 
     tc_time_zone_data_type = tcase_create("TimeZoneDataType");
+
+    tcase_add_checked_fixture(tc_time_zone_data_type, setup, NULL);
+
     tcase_add_test(tc_time_zone_data_type, test_time_zone_data_type);
     suite_add_tcase(s, tc_time_zone_data_type);
 
