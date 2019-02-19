bff03feaaf704bfd4228c0759d690bade2752097
Matthieu Archez <matthieu.archez@systerel.fr>
2019-02-19 11:48:45 +0100

    Ticket #565: refactoring check encodeable types

--- tests/helpers/check_encodeable_types.c
+++ tests/helpers/check_encodeable_types.c
@@ -39,48 +39,55 @@ static void setup(void)
     SOPC_Helper_EndiannessCfg_Initialize();
 }
 
+static void ck_assert_ok(SOPC_ReturnStatus status)
+{
+    ck_assert(SOPC_STATUS_OK == status);
+}
+
 /******************************************************************************
  * Generic checker for encodeable types
  * Test of Initialize, Decode and Encode functions
  * The type and the checker function are received as parameters, as well as the
  * buffer to be decoded.
  ******************************************************************************/
-static void check_encodeable_type(SOPC_EncodeableType* encodeable_type,
+static void check_encodeable_type(SOPC_EncodeableType* enc_type,
                                   void (*encodeable_type_checker)(void*),
                                   uint8_t* frame,
                                   uint32_t frame_size)
 {
     // Allocation
-    void* encodeable_object = calloc(1, encodeable_type->AllocationSize);
-    ck_assert_ptr_nonnull(encodeable_object);
+    void* obj = NULL;
+    SOPC_Buffer* input = NULL;
+    SOPC_Buffer* output = NULL;
 
     // Buffer initialization
-    SOPC_Buffer* buffer_input = SOPC_Buffer_Create(frame_size);
-    SOPC_Buffer* buffer_output = SOPC_Buffer_Create(frame_size);
+    input = SOPC_Buffer_Create(frame_size);
+    output = SOPC_Buffer_Create(frame_size);
 
-    ck_assert(SOPC_Buffer_Write(buffer_input, frame, frame_size) == SOPC_STATUS_OK);
-    ck_assert(SOPC_Buffer_SetPosition(buffer_input, 0) == SOPC_STATUS_OK);
+    ck_assert_ok(SOPC_Buffer_Write(input, frame, frame_size));
+    ck_assert_ok(SOPC_Buffer_SetPosition(input, 0));
 
     // Initialization
-    encodeable_type->Initialize(encodeable_object);
+    SOPC_Encodeable_Create(enc_type, &obj);
+    ck_assert_ptr_nonnull(obj);
 
     // Decode
-    ck_assert(encodeable_type->Decode(encodeable_object, buffer_input) == SOPC_STATUS_OK);
+    ck_assert_ok(enc_type->Decode(obj, input));
 
     // Check object content
-    encodeable_type_checker(encodeable_object);
+    encodeable_type_checker(obj);
 
     // Encode
-    ck_assert(encodeable_type->Encode(encodeable_object, buffer_output) == SOPC_STATUS_OK);
+    ck_assert_ok(enc_type->Encode(obj, output));
 
     // Check buffers
-    ck_assert_int_eq(buffer_output->position, frame_size);
-    ck_assert_mem_eq(buffer_input->data, buffer_output->data, frame_size);
+    ck_assert_uint_eq(output->position, frame_size);
+    ck_assert_mem_eq(input->data, output->data, frame_size);
 
     // Clear all objects
-    encodeable_type->Clear(encodeable_object);
-    SOPC_Buffer_Delete(buffer_input);
-    SOPC_Buffer_Delete(buffer_output);
+    SOPC_Encodeable_Delete(enc_type, &obj);
+    SOPC_Buffer_Delete(input);
+    SOPC_Buffer_Delete(output);
 }
 
 /******************************************************************************
@@ -89,20 +96,25 @@ static void check_encodeable_type(SOPC_EncodeableType* encodeable_type,
 
 static void time_zone_data_type_checker(void* encodeable_type_object)
 {
-    OpcUa_TimeZoneDataType* time_zone_data_type_object = encodeable_type_object;
+    OpcUa_TimeZoneDataType* obj = encodeable_type_object;
 
-    ck_assert(time_zone_data_type_object->encodeableType == &OpcUa_TimeZoneDataType_EncodeableType);
-    ck_assert(time_zone_data_type_object->Offset == -1);
-    ck_assert(time_zone_data_type_object->DaylightSavingInOffset == true);
+    ck_assert(obj->encodeableType == &OpcUa_TimeZoneDataType_EncodeableType);
+
+    // Check content
+    ck_assert(obj->Offset == -1);
+    ck_assert(obj->DaylightSavingInOffset == true);
 }
 
 START_TEST(test_time_zone_data_type)
 {
     // Test frame creation
-    uint8_t frame[] = {0xFF, 0xFF, 0x1};
-    uint32_t frame_size = (uint32_t) sizeof frame;
+    uint8_t frame[] = {0xFF, 0xFF, // Offset == -1
+                       0x1};       // DaylightSavingInOffset == true
 
-    check_encodeable_type(&OpcUa_TimeZoneDataType_EncodeableType, time_zone_data_type_checker, frame, frame_size);
+    check_encodeable_type(&OpcUa_TimeZoneDataType_EncodeableType,
+                          time_zone_data_type_checker,
+                          frame,
+                          (uint32_t) sizeof frame);
 }
 END_TEST
 
@@ -112,28 +124,42 @@ END_TEST
 
 static void aggregate_filter_result_checker(void* encodeable_type_object)
 {
-    OpcUa_AggregateFilterResult* aggregate_filter_result_object = encodeable_type_object;
+    OpcUa_AggregateFilterResult* obj = encodeable_type_object;
+
+    ck_assert(obj->encodeableType == &OpcUa_AggregateFilterResult_EncodeableType);
 
     // Check content
-    ck_assert_int_eq(aggregate_filter_result_object->RevisedStartTime, -1);
-    ck_assert_double_eq(aggregate_filter_result_object->RevisedProcessingInterval, 0);
+    ck_assert_int_eq(obj->RevisedStartTime,            -1);
+    ck_assert_double_eq(obj->RevisedProcessingInterval, 0);
 
     // Check content of nested encodeable type
-    ck_assert_int_eq(aggregate_filter_result_object->RevisedAggregateConfiguration.UseServerCapabilitiesDefaults, true);
-    ck_assert_int_eq(aggregate_filter_result_object->RevisedAggregateConfiguration.TreatUncertainAsBad, true);
-    ck_assert_int_eq(aggregate_filter_result_object->RevisedAggregateConfiguration.PercentDataBad, 0x2A);
-    ck_assert_int_eq(aggregate_filter_result_object->RevisedAggregateConfiguration.PercentDataGood, 0x3A);
-    ck_assert_int_eq(aggregate_filter_result_object->RevisedAggregateConfiguration.UseSlopedExtrapolation, true);
+    OpcUa_AggregateConfiguration nested_obj = obj->RevisedAggregateConfiguration;
+
+    ck_assert_int_eq(nested_obj.UseServerCapabilitiesDefaults,  true);
+    ck_assert_int_eq(nested_obj.TreatUncertainAsBad,            true);
+    ck_assert_uint_eq(nested_obj.PercentDataBad,                  42);
+    ck_assert_uint_eq(nested_obj.PercentDataGood,                 58);
+    ck_assert_int_eq(nested_obj.UseSlopedExtrapolation,         true);
 }
 
 START_TEST(test_aggregate_filter_result)
 {
     // Test frame creation (with cursor position reset)
-    uint8_t frame[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 8B SOPC_Date_Time
-                       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 8B double
-                       0x01, 0x01, 0x2A, 0x3A, 0x01};                  // 5B AggregateConfiguration
+    uint8_t frame[] = {// 8B RevisedStartTime == -1
+                       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                       // 8B double == 0
+                       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
 
-    check_encodeable_type(&OpcUa_AggregateFilterResult_EncodeableType, aggregate_filter_result_checker, frame,
+                       // 5B AggregateConfiguration
+                       0x01,  // UseServerCapabilitiesDefaults == true
+                       0x01,  // TreatUncertainAsBad           == true
+                       0x2A,  // PercentDataBad                == 42
+                       0x3A,  // PercentDataGood               == 58
+                       0x01}; // UseSlopedExtrapolation        == true                 
+
+    check_encodeable_type(&OpcUa_AggregateFilterResult_EncodeableType,
+                          aggregate_filter_result_checker,
+                          frame,
                           (uint32_t) sizeof frame);
 }
 END_TEST
@@ -144,37 +170,66 @@ END_TEST
 
 static void browse_path_checker(void* encodeable_type_object)
 {
-    OpcUa_BrowsePath* browse_path_object = encodeable_type_object;
+    OpcUa_BrowsePath* obj = encodeable_type_object;
+
+    ck_assert(obj->encodeableType == &OpcUa_BrowsePath_EncodeableType);
 
     // Check content
-    ck_assert_int_eq(browse_path_object->StartingNode.IdentifierType, 0);
-    ck_assert_int_eq(browse_path_object->StartingNode.Namespace, 4);
-    ck_assert_int_eq(browse_path_object->StartingNode.Data.Numeric, 3);
+    ck_assert_int_eq(obj->StartingNode.IdentifierType,  0);
+    ck_assert_int_eq(obj->StartingNode.Namespace,       4);
+    ck_assert_uint_eq(obj->StartingNode.Data.Numeric,   3);
 
     // Check content of nested encodeable type
-    ck_assert_int_eq(browse_path_object->RelativePath.NoOfElements, 2);
-    ck_assert_ptr_nonnull(browse_path_object->RelativePath.Elements);
+    ck_assert_int_eq(obj->RelativePath.NoOfElements,    2);
+    ck_assert_ptr_nonnull(obj->RelativePath.Elements);
 
     // Check content of Element of nested encodeable type)
-    ck_assert_int_eq(browse_path_object->RelativePath.Elements[0].ReferenceTypeId.Data.Numeric, 3);
-    ck_assert_int_eq(browse_path_object->RelativePath.Elements[0].IncludeSubtypes, true);
-    ck_assert_int_eq(browse_path_object->RelativePath.Elements[1].ReferenceTypeId.Data.Numeric, 171);
-    ck_assert_int_eq(browse_path_object->RelativePath.Elements[1].IsInverse, true);
+    OpcUa_RelativePathElement elem = obj->RelativePath.Elements[0];
+
+    ck_assert_int_eq(elem.ReferenceTypeId.IdentifierType,  0);
+    ck_assert_int_eq(elem.ReferenceTypeId.Namespace,       4);
+    ck_assert_uint_eq(elem.ReferenceTypeId.Data.Numeric,   3);
+    ck_assert_int_eq(elem.IsInverse,                   false);
+    ck_assert_int_eq(elem.IncludeSubtypes,              true);
+    ck_assert_uint_eq(elem.TargetName.NamespaceIndex,     10);
+
+    elem = obj->RelativePath.Elements[1];
+
+    ck_assert_int_eq(elem.ReferenceTypeId.IdentifierType,  0);
+    ck_assert_int_eq(elem.ReferenceTypeId.Namespace,       5);
+    ck_assert_uint_eq(elem.ReferenceTypeId.Data.Numeric, 171);
+    ck_assert_int_eq(elem.IsInverse,                    true);
+    ck_assert_int_eq(elem.IncludeSubtypes,             false);
+    ck_assert_uint_eq(elem.TargetName.NamespaceIndex,      0);
 }
 
 START_TEST(test_browse_path)
 {
     // Test frame creation
-    uint8_t frame[] = {0x01, 0x04, 0x03, 0x00, // BrowsePath->StartingNodeId
-                       0x02, 0x00, 0x00, 0x00, // BrowsePath->RelativePath->NoOfElements
-                       0x01, 0x04, 0x03, 0x00, // BrowsePath->RelativePath First element node id
-                       0x00, 0x01, 0x00, 0x00, // IncludeSubtypes true
+    uint8_t frame[] = {
+                       // BrowsePath->StartingNodeId
+                       0x01, 
+                       0x04, // Namespace == 4
+                       0x03, // Data == 3
+                       0x00, // IdentifierType == Numeric
+
+                       // BrowsePath->RelativePath
+                       0x02, 0x00, 0x00, 0x00, // NoOfElements == 2
+
+                       // BrowsePath->RelativePath First element
+                       0x01, 0x04, 0x03, 0x00, // ReferenceTypeId
+                       0x00, 0x01, 0x0A, 0x00, // IncludeSubtypes == true
                        0xff, 0xff, 0xff, 0xff,
-                       0x01, 0x05, 0xAB, 0x00, // BrowsePath->RelativePath Second element node id
-                       0x01, 0x00, 0x00, 0x00, // IsInverse true
+
+                       // BrowsePath->RelativePath Second element
+                       0x01, 0x05, 0xAB, 0x00, // ReferenceTypeId
+                       0x01, 0x00, 0x00, 0x00, // IsInverse == true
                        0xff, 0xff, 0xff, 0xff};
 
-    check_encodeable_type(&OpcUa_BrowsePath_EncodeableType, browse_path_checker, frame, (uint32_t) sizeof frame);
+    check_encodeable_type(&OpcUa_BrowsePath_EncodeableType,
+                          browse_path_checker,
+                          frame,
+                          (uint32_t) sizeof frame);
 }
 END_TEST
 
