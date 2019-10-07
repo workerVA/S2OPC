5a5f1c4298dfb11f628af3355018d8be8ba4b87a
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-10-07 09:38:30 +0200

    Ticket #599: Move all non mergeable specific freeRTOS and iMX6 files to pcb.aix.systerel.fr//git/c868/portage_freertos.git

--- tests/freertos/test_helpers_platform_dep/board_specific/mbedtls_port/ksdk/threading_alt.h
+++
@@ -1,9 +0,0 @@
-#ifndef KSDK_MBEDTLS_THREADING_ALT_H
-#define KSDK_MBEDTLS_THREADING_ALT_H
-
-#include "SOPC_Mutexes.h"
-#include "SOPC_Threads.h"
-
-typedef Mutex mbedtls_threading_mutex_t;
-
-#endif
